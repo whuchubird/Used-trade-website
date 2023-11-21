6e18a635327881990a523b922564ad9efa5f5376
HYOJEONG0213 <fredflem@naver.com>
2023-11-21 14:06:30 +0900

    양효정 - 기본 셋팅

---
+++ used-trade-website/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import type {NextPage} from "next";
+
+const Home: NextPage = () => {
+  return(
+   <div className="bg-red-500">
+    <h1 className="text-blue-500">it works</h1>
+    </div>
+  );
+};
+
+export default Home;
+
+/*import Image from 'next/image'
+import { Inter } from 'next/font/google'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return null;
+};
+
+*/
