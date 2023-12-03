365e8a7132d7cdda69dfeac4d84da6777ff193bf
HYOJEONG0213 <fredflem@naver.com>
2023-12-04 00:57:19 +0900

    양효정 - 로그인창->홈페이지창 연결
    쿠키 넘겨주기!!!! 힘들었따!!!

--- used-trade-website/components/input.tsx
+++ used-trade-website/components/input.tsx
@@ -1,9 +1,9 @@
-/*import type { UseFormRegisterReturn } from 'react-hook-form'
+import type { UseFormRegisterReturn } from 'react-hook-form'
 
 interface InputProps {
   label: string
   name: string
-  kind?: 'text' | 'phone' | 'price'
+  kind?: 'text' | 'price'
   type: string
   register: UseFormRegisterReturn
   required: boolean
@@ -53,21 +53,6 @@ export default function Input({
           </div>
         </div>
       ) : null}
-      {kind === 'phone' ? (
-        <div className="flex rounded-md shadow-sm">
-          <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
-            +82
-          </span>
-          <input
-            id={name}
-            required={required}
-            {...register}
-            type={type}
-            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-          />
-        </div>
-      ) : null}
     </div>
   )
 }
-*/
--- used-trade-website/libs/client/useMutation.tsx
+++ used-trade-website/libs/client/useMutation.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 
-interface UseMutationState {
+interface UseMutationState<T> {
   loading: boolean
-  data: undefined | any
-  error: undefined | any
+  data?: T
+  error?: Object
 }
-type UseMutationResult = [(data: any) => void, UseMutationState]
+type UseMutationResult<T> = [(data: any) => void, UseMutationState<T>]
 
 //api로 데이터를 POST하기
-export default function useMutation(url: string): UseMutationResult {
-  const [state, setState] = useState({
+export default function useMutation<T = any>(
+  url: string,
+): UseMutationResult<T> {
+  const [state, setState] = useState<UseMutationState<T>>({
     loading: false,
     data: undefined,
     error: undefined,
@@ -18,8 +20,12 @@ export default function useMutation(url: string): UseMutationResult {
   const [loading, setLoading] = useState(false)
   const [data, setData] = useState<undefined | any>(undefined)
   const [error, setError] = useState<undefined | any>(undefined)
+
   function mutation(data: any) {
     setLoading(true)
+
+    console.log('data1', data)
+
     fetch(url, {
       method: 'POST',
       headers: {
@@ -28,9 +34,16 @@ export default function useMutation(url: string): UseMutationResult {
       body: JSON.stringify(data),
     })
       .then((response) => response.json().catch(() => {}))
-      .then(setData)
-      .catch(setError)
-      .finally(() => setLoading(false))
+      .then((responseData) => {
+        setData(responseData)
+        setLoading(false)
+        console.log('data2', responseData)
+      })
+      .catch((error) => {
+        setError(error)
+        setLoading(false)
+      })
   }
+
   return [mutation, { loading, data, error }]
 }
